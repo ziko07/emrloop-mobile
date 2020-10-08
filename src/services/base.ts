7a2bd0d3e1fc778bed4ac91616ea8ab54df7b98f
Shah Shishir <shahshishir.09@gmail.com>
2020-10-08 16:52:42 +0600

    fix splash screen image

--- src/services/base.ts
+++ src/services/base.ts
@@ -1,9 +1,9 @@
 export class Base {
-    // static apiUrl = 'https://34.207.81.246/api/v1';
-    // static baseUrl = 'https://34.207.81.246/';
+    static apiUrl = 'https://34.207.81.246/api/v1';
+    static baseUrl = 'https://34.207.81.246/';
 
-    static apiUrl = 'http://93ec8bdc45f6.ngrok.io/api/v1';
-    static baseUrl = 'http://93ec8bdc45f6.ngrok.io/';
+    // static apiUrl = 'http://93ec8bdc45f6.ngrok.io/api/v1';
+    // static baseUrl = 'http://93ec8bdc45f6.ngrok.io/';
 
     static getAuthToken() {
         const auth = JSON.parse(window.localStorage.getItem('auth'));
