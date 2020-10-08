270462765c284ffa5d5afd0666e70bc91c228e74
Shah Shishir <shahshishir.09@gmail.com>
2020-10-08 19:34:51 +0600

    some update

--- src/services/base.ts
+++ src/services/base.ts
@@ -2,8 +2,8 @@ export class Base {
     static apiUrl = 'https://34.207.81.246/api/v1';
     static baseUrl = 'https://34.207.81.246/';
 
-    // static apiUrl = 'http://93ec8bdc45f6.ngrok.io/api/v1';
-    // static baseUrl = 'http://93ec8bdc45f6.ngrok.io/';
+    // static apiUrl = 'https://022c14f1ecdb.ngrok.io/api/v1';
+    // static baseUrl = 'https://022c14f1ecdb.ngrok.io';
 
     static getAuthToken() {
         const auth = JSON.parse(window.localStorage.getItem('auth'));
